083b9c82eb1f74ba52beb74394e184542d20694e
Till-I-Die <twerkmueller@gmail.com>
2024-09-13 18:15:32 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
-let H5PInhalt: any = undefined;
+let geographyWebsite: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
@@ -24,25 +24,15 @@ WA.onInit().then(() => {
     WA.room.area.onLeave('clock').subscribe(closePopup)
 
     WA.room.area.onEnter('geographyWebsite').subscribe(async () => {
-        H5PInhalt = await WA.nav.openCoWebSite(`${root}/HI/geography.html`, true, "", 69)
+        geographyWebsite = await WA.nav.openCoWebSite(`${root}/HI/tool.html`, true, "", 60)
     })
 
     WA.room.area.onLeave('geographyWebsite').subscribe(() => {
-        if (H5PInhalt !== undefined) {
-            H5PInhalt.close();
+        if (geographyWebsite !== undefined) {
+            geographyWebsite.close();
         }
     })
     
-    WA.room.area.onEnter('Inhalt').subscribe(async () => {
-        H5PInhalt = await WA.nav.openCoWebSite(`${root}/HI/tool.html`, true, "", 69)
-    })
-
-    WA.room.area.onLeave('Inhalt').subscribe(() => {
-        if (H5PInhalt !== undefined) {
-            H5PInhalt.close();
-        }
-    })
-
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
