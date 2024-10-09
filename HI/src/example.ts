ab64314d60f3304d114af1c656f69e0c992f1658
Till-I-Die <twerkmueller@gmail.com>
2024-10-09 17:48:40 +0200

    H5P ergänzung

---
+++ HI/src/example.ts
@@ -0,0 +1,11 @@
+/// <reference types="@workadventure/iframe-api-typings" />
+
+window.onload = async () => {
+    try {
+        WA.onInit().then(() => {
+            console.log("WA ready to go!")
+        } catch (error) {
+            console.error("Error from WA API:", error);
+        }
+    };
+}
